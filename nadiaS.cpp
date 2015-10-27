46be712606fd7014975775e1fec0d81fe86d608f
nsalem1 <nsalem@cs.csub.edu>
2015-10-26 18:31:38 -0700

    Update nadiaS.cpp

--- nadiaS.cpp
+++ nadiaS.cpp
@@ -1,6 +1,30 @@
 /*Nadia Salem*/
-
-
+int random(int x, int y)
+{
+     srand(time(NULL));
+    int num;
+    if(x < 0 && y < 0)
+    {
+            num = rand()%((y-x))+y;
+    }
+    else if (x > 0 && y > 0)
+    {
+        if(x < y)
+        {
+            num = rand() % (x-y)+ x;
+        }
+        else
+        num = rand() % (x-y) + y;
+    }
+    else
+    {
+        if(x < y)
+           num = rand() % ((-1*x)+y) +x;
+        else
+        num = rand() % ((-1*y)+x)+y;
+    }
+return num;
+}
 void drawPlatform(Game *game, int amt)
 {
     glColor3ub(255,255,255);
@@ -27,14 +51,14 @@ void drawPlatform(Game *game, int amt)
 // Pedro Gonzalez: "this will cause issue if amt is greater than 5!!!"
 //Nadia Salem: I fixed Drawplatforms so it won't happen
 
-void makePlatform(int amt, Game *game, int centerX)
+void makePlatform(int amt, Game *game, int startCenterX)
 {
     
     for(int i = 0; i<amt; i++)
     {
         game->platform[i].width=50;
         game->platform[i].height=10;
-        game->platform[i].pos.x=centerX +30*i;
+        game->platform[i].pos.x=startCenterX +random(-100,100) +30*i;
         game->platform[i].pos.y=120+5*65-50*i;
     }
 }

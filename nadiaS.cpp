c963a717e918002fd33fef2967a69bc3c5aa1b03
nsalem1 <nsalem@cs.csub.edu>
2015-10-28 17:48:16 -0700

    Update nadiaS.cpp

--- nadiaS.cpp
+++ nadiaS.cpp
@@ -6,7 +6,7 @@ void drawPlatform(Game *game, int amt)
 
     
     float w, h;
-    for(int j = 0; j <1; j++)
+    for(int j = 0; j <2; j++)
     {
         glPushMatrix();
         glTranslatef(game->platform[j].pos.x,game->platform[j].pos.y, 0);
@@ -29,11 +29,11 @@ void drawPlatform(Game *game, int amt)
 void makePlatform(int amt, Game *game)
 {
     srand(time(NULL));
-    for(int i = 0; i<1; i++)
+    for(int i = 0; i<2; i++)
     {
         game->platform[i].width=50;
         game->platform[i].height=10;
         game->platform[i].pos.x=window_width/(2);
-        game->platform[i].pos.y=window_height/2;
+        game->platform[i].pos.y=window_height/2-5*i;
     }
 }

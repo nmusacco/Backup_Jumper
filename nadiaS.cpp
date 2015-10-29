365d2b31295aa4f2facb8c364a87032b1484db3a
nsalem1 <nsalem@cs.csub.edu>
2015-10-28 17:57:50 -0700

    Update nadiaS.cpp

--- nadiaS.cpp
+++ nadiaS.cpp
@@ -34,6 +34,6 @@ void makePlatform(int amt, Game *game)
         game->platform[i].width=50;
         game->platform[i].height=10;
         game->platform[i].pos.x=window_width/(2);
-        game->platform[i].pos.y=(window_height+i)/4;
+        game->platform[i].pos.y=(window_height+i+1)/4;
     }
 }

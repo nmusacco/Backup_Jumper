51b1883dff7f9dfb310b63f3dad92a831b0e6496
Anthony <panthony500@gmail.com>
2015-11-02 19:08:13 -0800

    set some values for width/height of platforms and set the randomized x value of platforms

--- nadiaS.cpp
+++ nadiaS.cpp
@@ -3,8 +3,7 @@
 void drawPlatform(Game *game, int amt)
 {
     glColor3ub(255,255,255);
-
-    
+	
     float w, h;
     for(int j = 0; j <amt; j++)
     {
@@ -22,18 +21,26 @@ void drawPlatform(Game *game, int amt)
     }
 }
 
-
-// Pedro Gonzalez: "this will cause issue if amt is greater than 5!!!"
+// HEY!!! (10/28/2015)
+// =====================================================================
+// Pedro Gonzalez: "changed some stuff so the heights and widths are 
+// 					tied with player's width and height"
+// =====================================================================
 //Nadia Salem: I fixed Drawplatforms so it won't happen
 
 void makePlatform(int amt, Game *game)
-{
-    srand(time(NULL));
+{   
+	int width = game->player.width * 15;
+	int height = game->player.height * 0.35;
+	srand(time(NULL));
     for(int i = 0; i<amt; i++)
     {
-        game->platform[i].width=50;
-        game->platform[i].height=10;
-        game->platform[i].pos.x=rand()%window_width;
-        game->platform[i].pos.y=(window_height-(i+1)*window_height/amt);
+        game->platform[i].width =  width; 
+        game->platform[i].height= height;
+        game->platform[i].pos.x= rand() % window_width;
+        //game->platform[i].pos.y=(window_height-(i+1)*window_height/amt);
+		
+	// h_w - h_w/5 * (i + 1) + player height * 2
+	game->platform[i].pos.y = window_height - 1.0/6.0 *window_height* (i + 1) + game->platform[i].height*2;
     }
 }

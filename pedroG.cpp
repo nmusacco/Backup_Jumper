82e8af20879e19e33510a16881281d3d378e4e35
Anthony <panthony500@gmail.com>
2015-11-11 23:28:05 -0800

    scrolling background

--- pedroG.cpp
+++ pedroG.cpp
@@ -174,8 +174,8 @@ float b_i = 0.0;
 void drawBackground(Game * game)
 {
 	float x = 0.001;
-	if(b_i >= 0.3 + b_i)
-		b_i = 0.00;
+	//if(b_i >= 0.3 + b_i)
+	//	b_i = 0.00;
 		
 	//*/
 	//int width = background->width;
@@ -183,14 +183,14 @@ void drawBackground(Game * game)
 	glColor3f(1.0, 1.0, 1.0);
 	glBindTexture(GL_TEXTURE_2D, backgroundTexture);
 	glBegin(GL_QUADS);
-	glTexCoord2f(0, 1);
+	glTexCoord2f(0, 0.03 + b_i); // bot left
 		glVertex2i(0,0);
-	glTexCoord2f(0,0);
-		glVertex2i(0, window_height);//height/2);//window_height);
-	glTexCoord2f(1,0);
-		glVertex2i(window_width, window_height);//width/2, height/2);//window_width, window_height);
-	glTexCoord2f(1, 1);
-		glVertex2i(window_width, 0);//width/2,0);//window_width, 0);
+	glTexCoord2f(0,b_i); // top left
+		glVertex2i(0, window_height);
+	glTexCoord2f(1,b_i); // top right
+		glVertex2i(window_width, window_height);
+	glTexCoord2f(1, 0.03 + b_i); // bot right
+		glVertex2i(window_width, 0);
 	glEnd();
 
 	

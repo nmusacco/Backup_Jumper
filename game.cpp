ad9d1f58b51f92f0d5254596d1db31bef2da8d9c
nsalem1 <nsalem@cs.csub.edu>
2015-11-12 11:31:37 -0800

    comments and fixed the indentation problem

--- game.cpp
+++ game.cpp
@@ -14,211 +14,212 @@
 
 Game::Game()
 {
-	setPos(30,1000);
-	setAccel(0,0);
-	gravity = 1;
+    setPos(30,1000);
+    setAccel(0,0);
+    gravity = 1;
 
-	powerupTimer = 14;
+    powerupTimer = 14;
 
-	powerups = NULL;
-	//missiles = NULL;
+    powerups = NULL;
+    //missiles = NULL;
 
-	accelY(-1);
-	if_jump = false;
-	if_hit = false;
-	run = true;
-	guts = false;
-	missiles.numExp = 0;
-	
+    accelY(-1);
+    if_jump = false;
+    if_hit = false;
+    run = true;
+    guts = false;
+    missiles.numExp = 0;
+
 }
 
 // create ONE missile
 void Game::createMissiles()
 {
 
-    	
-    	setMissiles = true;
-	missiles.numExp = 0;
-	clock_gettime(CLOCK_REALTIME, &missiles.MissilesStart);
-	missiles.nextframe = 0;
-	int x = this->window_width/2;
-	int y = this->window_height/2;
-	
-	int width = 3*player.width;
-	int height = 2*player.height;
-	
-	missiles.position.x = x;
-	missiles.position.y = y;
-	missiles.width = width;
-	missiles.height = height;
 
-	cout << "missile created" << endl;
+    setMissiles = true;
+    missiles.numExp = 0;
+    clock_gettime(CLOCK_REALTIME, &missiles.MissilesStart);
+    missiles.nextframe = 0;
+    int x = this->window_width/2;
+    int y = this->window_height/2;
+
+    int width = 3*player.width;
+    int height = 2*player.height;
+
+    missiles.position.x = x;
+    missiles.position.y = y;
+    missiles.width = width;
+    missiles.height = height;
+
+    cout << "missile created" << endl;
 }
 
 
 
 bool Game::inAir()
 {
-	//if(checkCollision())
-		//return false;
-	
-	if(player.position.y > 0 && if_jump == true)
-	{
-		if_jump = false;
-		return true;
-	}
-	return false;
+    //if(checkCollision())
+    //return false;
+
+    if(player.position.y > 0 && if_jump == true)
+    {
+	if_jump = false;
+	return true;
+    }
+    return false;
 }
 
 void Game::applyGravity()
 {
-	if(player.position.y - player.height > 0)
-		accelY(-0.25 * gravity);
-	
-	Powerup * p = powerups;
-	while(p != NULL)
-	{
-		if(p->position.y - p->height > 0)
-		p->velocity.y += -0.25 * gravity;
-		p = p->next;
-	}
+    if(player.position.y - player.height > 0)
+	accelY(-0.25 * gravity);
+
+    Powerup * p = powerups;
+    while(p != NULL)
+    {
+	if(p->position.y - p->height > 0)
+	    p->velocity.y += -0.25 * gravity;
+	p = p->next;
+    }
 }
 
 void Game::missileChasePlayer()
 {
-	if(!setMissiles)
-	    return;
-	float x = missiles.position.x;
-	float y = missiles.position.y;
-	float speed = 2;
-	float x_vel;
-	float y_vel;
+    if(!setMissiles)
+	return;
+    float x = missiles.position.x;
+    float y = missiles.position.y;
+    float speed = 2;
+    float x_vel;
+    float y_vel;
 
-	missiles.nextframe++;
-	
-	if(posX() > x)   
-		x_vel = speed;
-	else if(posX() == x)
-		x_vel = 0;
-	else	
-		x_vel = -speed;
-	
-	if(posY() > y)
-		y_vel = speed;
-	else if(posY() == y)
-		y_vel = 0;
-	else
-		y_vel = -speed;
+    missiles.nextframe++;
 
-	missiles.velocity.x = x_vel;
-	missiles.velocity.y = y_vel;
+    if(posX() > x)   
+	x_vel = speed;
+    else if(posX() == x)
+	x_vel = 0;
+    else	
+	x_vel = -speed;
+
+    if(posY() > y)
+	y_vel = speed;
+    else if(posY() == y)
+	y_vel = 0;
+    else
+	y_vel = -speed;
+
+    missiles.velocity.x = x_vel;
+    missiles.velocity.y = y_vel;
 }
 
 void Game::setGravity(int g)
 {
-	gravity = g;
+    gravity = g;
 }
 
 
 float Game::posX()
 {
-	return player.position.x;
+    return player.position.x;
 }
 
 float Game::posY()
 {
-	return player.position.y;
+    return player.position.y;
 }
 
 
 float Game::velY()
 {
-	return player.velocity.y;
+    return player.velocity.y;
 }
 
 float Game::velX()
 {
-	return player.velocity.x;
+    return player.velocity.x;
 }
 
 // TODO fix gravity when window resizes
 void Game::setResolution(int x, int y)
 {
-	window_width = x;
-	window_height = y;
-	player.width = window_height*0.01;
-	player.height = window_height*0.05;
-	missiles.width = 3*player.width;
-	missiles.height = 2*3*player.height;
-	
-	
-	
-	for(int i = 0; i < 5; ++i)
-	{
-		platform[i].width = player.width * 15;
-		platform[i].height = player.height * 0.35;
-		//platform[i].pos.y += 1.0/6.0 *window_height* (i + 1) + platform[i].height*2;
-	}
+    window_width = x;
+    window_height = y;
+    player.width = window_height*0.01;
+    player.height = window_height*0.05;
+    missiles.width = 3*player.width;
+    missiles.height = 2*3*player.height;
+
+
+
+    for(int i = 0; i < 5; ++i)
+    {
+	platform[i].width = player.width * 15;
+	platform[i].height = player.height * 0.35;
+	//platform[i].pos.y += 1.0/6.0 *window_height* (i + 1) + platform[i].height*2;
+    }
 }
 
 
 void Game::setPos(float x = 0, float y = 0)
 {
-	player.position.x = x;
-	player.position.y = y;
+    player.position.x = x;
+    player.position.y = y;
 }
 
 void Game::setPosX(float x)
 {
-	player.position.x = x;
+    player.position.x = x;
 }
 
 void Game::setPosY(float y)
 {
-	player.position.y = y;
+    player.position.y = y;
 }
 
 void Game::setAccel(float x, float y)
 {
-	player.velocity.x = x;
-	player.velocity.y = y;
+    player.velocity.x = x;
+    player.velocity.y = y;
 }
 
 void Game::accelX(float x)
 {
-	player.velocity.x += x;
+    player.velocity.x += x;
 }
 
 void Game::accelY(float y)
 {
-	player.velocity.y += y;
+    player.velocity.y += y;
 }
 
 void Game::move()
 {
-	player.position.x += player.velocity.x;
-	player.position.y += player.velocity.y;
-	
-	missiles.position.x += missiles.velocity.x;
-	missiles.position.y += missiles.velocity.y;
+    player.position.x += player.velocity.x;
+    player.position.y += player.velocity.y;
 
-	//player.position.y -= 0.1 *player.height;
-	for(int i = 0; i < 5; ++i)
-			platform[i].pos.y -= 0.04 * player.height;
+    missiles.position.x += missiles.velocity.x;
+    missiles.position.y += missiles.velocity.y;
+
+    //player.position.y -= 0.1 *player.height;
+    for(int i = 0; i < 5; ++i)
+	platform[i].pos.y -= 0.04 * player.height;
 }
 
 void Game::updatePlatforms()
 {
-	//int width = player.width * 15;
-	//int height = player.height * 0.35;
-	for(int i = 0; i < 5; ++i)
-		if(platform[i].pos.y < 0)
-		{
-			srand(time(NULL));
-			platform[i].pos.x= rand()%window_width-rand()%platform[i].width;
-			// h_w - h_w/5 * (i + 1) + player height * 2
-			platform[i].pos.y = window_height + platform[i].height;//-20;//window_height - 1.0/6.0 *window_height* (i + 1) + platform[i].height*2;
-		}
+    //int width = player.width * 15;
+    //int height = player.height * 0.35;
+    //when one platform hits the ground it is replaced and put on top
+    for(int i = 0; i < 5; ++i)
+	if(platform[i].pos.y < 0)
+	{
+	    srand(time(NULL));
+	    platform[i].pos.x= rand()%window_width-rand()%platform[i].width;
+	    // h_w - h_w/5 * (i + 1) + player height * 2
+	    platform[i].pos.y = window_height + platform[i].height;//-20;//window_height - 1.0/6.0 *window_height* (i + 1) + platform[i].height*2;
+	}
 }
 
 #include "brandiD.cpp"
@@ -228,5 +229,5 @@ void Game::updatePlatforms()
 // deallocate here!
 Game::~Game()
 {
-	
+
 }
--- nadiaS.cpp
+++ nadiaS.cpp
@@ -1,7 +1,8 @@
-/*Nadia Salem*/
+/*Nadia Salem
+ * this is to draw and make platforms for the game
+ * */
 Ppmimage *platform = NULL;
 GLuint platformTexture;
-
 
 void loadTexture()
 {
@@ -12,75 +13,71 @@ void loadTexture()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexImage2D(GL_TEXTURE_2D, 0, 3, platform->width, platform->height,
-			0, GL_RGB, GL_UNSIGNED_BYTE, platform->data);
+	    0, GL_RGB, GL_UNSIGNED_BYTE, platform->data);
 }
+
 
 void drawPlatform(Game *game, int amt)
 {
-	float w, h;
-    	
-    	if(setbackground)
-		for(int j = 0; j <amt; j++)
-		{
-			
-		    glPushMatrix();
-			glColor3ub(255,255,255);
-		    glTranslatef(game->platform[j].pos.x,game->platform[j].pos.y,0);
-		    w = game->platform[j].width;
-		    h = game->platform[j].height;
-		    glBindTexture(GL_TEXTURE_2D, platformTexture);
-		    glBegin(GL_QUADS);
-		    glTexCoord2f(0,1); // bottom left
-		    glVertex2i(-w,-h);
-		    glTexCoord2f(0,0); // top left
-		    glVertex2i(-w,h);
-		    glTexCoord2f(1,0); // top right
-		    glVertex2i(w,h);
-		    glTexCoord2f(1,1); // bottom right
-		    glVertex2i(w,-h);
-		    glEnd();
-		    glPopMatrix();
-		}
-	else
-	    for(int j = 0; j < amt; j++)
-	    {
-		
-	    	glPushMatrix();
-		glColor3ub(255,255,255);
-		glTranslatef(game->platform[j].pos.x,game->platform[j].pos.y, 0);
-		w=game->platform[j].width;
-		h=game->platform[j].height;
-		glBegin(GL_QUADS);
-		glVertex2i(-w,-h);
-		glVertex2i(-w,h);
-		glVertex2i(w,h);
-		glVertex2i(w,-h);
-		glEnd();
-		glPopMatrix();
-	    }
-}
+    float w, h;
+    //check to see if we are in unit test mode or in game mode. game mode when the if is true
+    if(setbackground)
+	for(int j = 0; j <amt; j++)
+	{
 
-// HEY!!! (10/28/2015)-
-// =====================================================================
-// Pedro Gonzalez: "changed some stuff so the heights and widths are 
-// 			tied with player's width and height"
-// =====================================================================
-//Nadia Salem: I fixed Drawplatforms so it won't happen
+	    glPushMatrix();
+	    glColor3ub(255,255,255);
+	    glTranslatef(game->platform[j].pos.x,game->platform[j].pos.y,0);
+	    w = game->platform[j].width;
+	    h = game->platform[j].height;
+	    glBindTexture(GL_TEXTURE_2D, platformTexture);
+	    glBegin(GL_QUADS);
+	    glTexCoord2f(0,1); // bottom left
+	    glVertex2i(-w,-h);
+	    glTexCoord2f(0,0); // top left
+	    glVertex2i(-w,h);
+	    glTexCoord2f(1,0); // top right
+	    glVertex2i(w,h);
+	    glTexCoord2f(1,1); // bottom right
+	    glVertex2i(w,-h);
+	    glEnd();
+	    glPopMatrix();
+	}
+    else
+	for(int j = 0; j < amt; j++)
+	{
+
+	    glPushMatrix();
+	    glColor3ub(255,255,255);
+	    glTranslatef(game->platform[j].pos.x,game->platform[j].pos.y, 0);
+	    w=game->platform[j].width;
+	    h=game->platform[j].height;
+	    glBegin(GL_QUADS);
+	    glVertex2i(-w,-h);
+	    glVertex2i(-w,h);
+	    glVertex2i(w,h);
+	    glVertex2i(w,-h);
+	    glEnd();
+	    glPopMatrix();
+	}
+}
 
 void makePlatform(int amt, Game *game)
 {   
-	int width = game->player.width * 15;
-	int height = game->player.height * 0.35;
-	srand(time(NULL));
-	for(int i = 0; i<amt; i++)
-	{
-	    game->platform[i].width =  width; 
-	    game->platform[i].height= height;
-	    game->platform[i].pos.x= rand() % window_width;
-	    //game->platform[i].pos.y=(window_height-(i+1)*window_height/amt);
+    //connects platform with player's width and height
+    int width = game->player.width * 15;
+    int height = game->player.height * 0.35;
+    srand(time(NULL));
+    // designs platform and puts them up randomly on width and 1/5 the screen on height
+    for(int i = 0; i<amt; i++)
+    {
+	game->platform[i].width =  width; 
+	game->platform[i].height= height;
+	game->platform[i].pos.x= rand() % window_width;
+	//game->platform[i].pos.y=(window_height-(i+1)*window_height/amt);
 
-	    // h_w - h_w/5 * (i + 1) + player height * 2
-	    game->platform[i].pos.y = window_height - 1.0/5.0 *window_height* (i + 1) + game->platform[i].height*2;
-	}
+	// h_w - h_w/5 * (i + 1) + player height * 2
+	game->platform[i].pos.y = window_height - 1.0/5.0 *window_height* (i + 1) + game->platform[i].height*2;
+    }
 }
 
